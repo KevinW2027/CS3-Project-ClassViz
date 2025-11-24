7cb6850d24d3f30a4e8a0d4ce93356decf73b509
KevinW2027 <kevin.wang2027@gbwl.org>
2025-11-24 17:40:44 +0000

    commit

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("hello world")
+import pandas as pd 
+import numpy as np
+from matplotlib import pyplot as plt
 
+# set style theme
+plt.style.use('dark_background')
 
-if __name__ == "__main__":
-    main()
+#read csv into dataframe
+df = pd.read_csv('ClassDataset.csv')
+df = df.head(8)
+print(df.info)
+
+# Group by neighborhood and compute the average
+avg_df = df.groupby('Neighborhood')['Commute Time Minutes'].mean()
+plt.bar(avg_df.index,avg_df.values, width= 0.6)
+
+plt.xlabel('Neighborhood')
+plt.ylabel('Commute time Minutes')
+plt.savefig('barchart.png', bbox_inches='tight')
+plt.close()
